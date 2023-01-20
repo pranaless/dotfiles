aa0ea81e5ee98a9af73206abbd73c20190c48873
nogerine <nogerine@tutanota.com>
2023-01-20 17:25:00 +0000

    modify mkHost to accept multiple hosts

--- flake.nix
+++ flake.nix
@@ -22,65 +22,66 @@
   in {
     inherit lib;
   
-    nixosConfigurations = lib.mkHost {
-      hostName = "humus";
-      system = "x86_64-linux";
-      modules = [
-        hyprland.nixosModules.default
-        ./users/humus
-        {
-          nixpkgs.config.allowUnfree = true;
-          nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-            "osu-lazer"
-            "steam"
-          ];
-          system.stateVersion = "22.05";
-        }
-        ({ pkgs, ... }: {
-          imports = [ # Include the results of the hardware scan.
-            ./hardware-configuration.nix
-          ];
+    nixosConfigurations = lib.mkHosts {
+      humus = {
+        system = "x86_64-linux";
+        modules = [
+          hyprland.nixosModules.default
+          ./users/humus
+          {
+            nixpkgs.config.allowUnfree = true;
+            nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+              "osu-lazer"
+              "steam"
+            ];
+            system.stateVersion = "22.05";
+          }
+          ({ pkgs, ... }: {
+            imports = [ # Include the results of the hardware scan.
+              ./hardware-configuration.nix
+            ];
 
-          # Not sure if this actually helps?
-          boot.initrd.availableKernelModules = [
-            "aesni_intel"
-            "cryptd"
-          ];
+            # Not sure if this actually helps?
+            boot.initrd.availableKernelModules = [
+              "aesni_intel"
+              "cryptd"
+            ];
 
-          fileSystems = {
-            "/".options = [ "compress=zstd" ];
-            "/home".options = [ "compress=zstd" ];
-            "/nix".options = [ "compress=zstd" "noatime" ];
-          };
-          swapDevices = [ { device = "/swap"; } ];
+            fileSystems = {
+              "/".options = [ "compress=zstd" ];
+              "/home".options = [ "compress=zstd" ];
+              "/nix".options = [ "compress=zstd" "noatime" ];
+            };
+            swapDevices = [ { device = "/swap"; } ];
 
-          environment.systemPackages = with pkgs; [
-            btrfs-progs
-          ];
+            environment.systemPackages = with pkgs; [
+              btrfs-progs
+            ];
 
-          time.timeZone = "UTC";
-          i18n.defaultLocale = "en_US.UTF-8";
-        })
-        ({ pkgs, ... }: {
-          networking.networkmanager.enable = true;
+            time.timeZone = "UTC";
+            i18n.defaultLocale = "en_US.UTF-8";
+          })
+          ({ pkgs, ... }: {
+            networking.networkmanager.enable = true;
 
-          services.printing = {
-            enable = true;
-            drivers = [ pkgs.brlaser ];
-          };
+            services.printing = {
+              enable = true;
+              drivers = [ pkgs.brlaser ];
+            };
 
-          services.openssh = {
-            enable = true;
-          };
+            services.openssh = {
+              enable = true;
+            };
           
-          environment.systemPackages = with pkgs; [
-            winetricks
-            wineWowPackages.waylandFull
-          ];
-          programs.hyprland.enable = true;
-          programs.steam.enable = true;
-        })
-      ];
+            environment.systemPackages = with pkgs; [
+              winetricks
+              wineWowPackages.waylandFull
+            ];
+            programs.hyprland.enable = true;
+            programs.steam.enable = true;
+          })
+        ];
+      };
     };
   };
 }
--- lib/default.nix
+++ lib/default.nix
@@ -6,42 +6,38 @@ rec {
 
   inherit (options) super;
 
-  mkHost = {
-    hostName,
+  mkHosts = builtins.mapAttrs (hostName: {
     system,
     modules ? {}
-  }:
-  {
-    ${hostName} = nixosSystem {
-      inherit system;
-      modules = [
-        home-manager.nixosModules.default
-        ../modules
-        ({ pkgs, lib, ... }: {
-          config = {
-            _module.args.dlib = self.lib;
-            home-manager.sharedModules = [{
-              config._module.args.dlib = self.lib;
-            }];
-            
-            nix.settings = {
-              experimental-features = [ "nix-command" "flakes" ];
-              sandbox = true;
-            };
-            home-manager = {
-              useUserPackages = true;
-              useGlobalPkgs = true;
-            };
-
-            system.configurationRevision = mkIf (self ? rev) self.rev;
-
-            boot.loader.systemd-boot.enable = true;
-            boot.loader.efi.canTouchEfiVariables = true;
-      
-            networking.hostName = hostName;
+  }: nixosSystem {
+    inherit system;
+    modules = [
+      home-manager.nixosModules.default
+      ../modules
+      ({ pkgs, lib, ... }: {
+        config = {
+          _module.args.dlib = self.lib;
+          home-manager.sharedModules = [{
+            config._module.args.dlib = self.lib;
+          }];
+          
+          nix.settings = {
+            experimental-features = [ "nix-command" "flakes" ];
+            sandbox = true;
           };
-        })
-      ] ++ modules;
-    };
-  };
+          home-manager = {
+            useUserPackages = true;
+            useGlobalPkgs = true;
+          };
+
+          system.configurationRevision = mkIf (self ? rev) self.rev;
+
+          boot.loader.systemd-boot.enable = mkDefault true;
+          boot.loader.efi.canTouchEfiVariables = mkDefault true;
+    
+          networking.hostName = hostName;
+        };
+      })
+    ] ++ modules;
+  });
 }
