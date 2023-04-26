192757379975ddd7b10b7c2c1652fa11f26be64f
nogerine <nogerine@tutanota.com>
2023-04-26 12:56:18 +0000

    factor host configuration out of flake.nix

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland }:
+  outputs = { self, nixpkgs, home-manager, hyprland }@inputs:
   let
     lib = import ./lib {
       inherit self home-manager;
@@ -73,61 +73,7 @@
     inherit lib;
   
     nixosConfigurations = mkHosts {
-      humus = {
-        system = "x86_64-linux";
-        # flakes = { };
-        modules = [
-          hyprland.nixosModules.default
-          ./users/humus
-          {
-            nixpkgs.config.allowUnfree = true;
-            nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-              "osu-lazer"
-              "steam"
-            ];
-            system.stateVersion = "22.05";
-          }
-          ({ pkgs, ... }: {
-            imports = [ # Include the results of the hardware scan.
-              ./hardware-configuration.nix
-            ];
-
-            # Not sure if this actually helps?
-            boot.initrd.availableKernelModules = [
-              "aesni_intel"
-              "cryptd"
-            ];
-
-            fileSystems = {
-              "/".options = [ "compress=zstd" ];
-              "/home".options = [ "compress=zstd" ];
-              "/nix".options = [ "compress=zstd" "noatime" ];
-            };
-            swapDevices = [ { device = "/swap"; } ];
-
-            environment.systemPackages = with pkgs; [
-              btrfs-progs
-            ];
-
-            time.timeZone = "UTC";
-            i18n.defaultLocale = "en_US.UTF-8";
-          })
-          ({ pkgs, ... }: {
-            networking.networkmanager.enable = true;
-
-            services.printing = {
-              enable = true;
-              drivers = [ pkgs.brlaser ];
-            };
-
-            services.openssh = {
-              enable = true;
-            };
-
-            programs.steam.enable = true;
-          })
-        ];
-      };
+      humus = import ./hosts/humus inputs;
     };
   };
 }
---
+++ hosts/humus/default.nix
@@ -0,0 +1,56 @@
+{ self, nixpkgs, hyprland, ... }:
+{
+  # hostName = "humus";
+  system = "x86_64-linux";
+  modules = [
+    hyprland.nixosModules.default
+    ../../users/humus
+    {
+      nixpkgs.config.allowUnfree = true;
+      nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+        "osu-lazer"
+        "steam"
+      ];
+      system.stateVersion = "22.05";
+    }
+    ({ pkgs, ... }: {
+      imports = [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+      ];
+
+      # Not sure if this actually helps?
+      boot.initrd.availableKernelModules = [
+        "aesni_intel"
+        "cryptd"
+      ];
+
+      fileSystems = {
+        "/".options = [ "compress=zstd" ];
+        "/home".options = [ "compress=zstd" ];
+        "/nix".options = [ "compress=zstd" "noatime" ];
+      };
+      swapDevices = [ { device = "/swap"; } ];
+
+      environment.systemPackages = with pkgs; [
+        btrfs-progs
+      ];
+
+      time.timeZone = "UTC";
+      i18n.defaultLocale = "en_US.UTF-8";
+    })
+    ({ pkgs, ... }: {
+      networking.networkmanager.enable = true;
+
+      services.printing = {
+        enable = true;
+        drivers = [ pkgs.brlaser ];
+      };
+
+      services.openssh = {
+        enable = true;
+      };
+
+      programs.steam.enable = true;
+    })
+  ];
+}
