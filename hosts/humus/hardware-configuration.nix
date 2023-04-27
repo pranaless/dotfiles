b71abdb61d093be1250af5e98fff51027780ba81
nogerine <nogerine@tutanota.com>
2023-04-27 13:57:05 +0000

    okay

--- hosts/humus/hardware-configuration.nix
+++ hosts/humus/hardware-configuration.nix
@@ -48,5 +48,6 @@
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
